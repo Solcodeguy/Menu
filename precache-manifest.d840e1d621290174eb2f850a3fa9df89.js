b2522e954cda1bb5b96b79e86d7c9544847341ed
Shawn A Meighn <smeighn@gmail.com>
2021-01-29 01:03:04 -0500

    Updates

---
+++ precache-manifest.d840e1d621290174eb2f850a3fa9df89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a16c0ab570ec2fcb9bb1c993b319ecce",
+    "url": "/github.io/Menu/index.html"
+  },
+  {
+    "revision": "013bdae372aa7538c17e",
+    "url": "/github.io/Menu/static/css/main.99af19dc.chunk.css"
+  },
+  {
+    "revision": "c5306faf86015639ba03",
+    "url": "/github.io/Menu/static/js/2.194d5c1d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github.io/Menu/static/js/2.194d5c1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "013bdae372aa7538c17e",
+    "url": "/github.io/Menu/static/js/main.aeb2090f.chunk.js"
+  },
+  {
+    "revision": "232087f10e7ffd32f14b",
+    "url": "/github.io/Menu/static/js/runtime-main.427f3a3c.js"
+  }
+]);
